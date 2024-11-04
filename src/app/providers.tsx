73c9c1ba7d3d539e6d6bfaca956eeab10ba40e4a
Elin Östman <elin.ostman@hotmail.com>
2024-11-04 10:39:56 +0100

    initiera property provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import { UserProvider } from "@/context/user";
+import { PropertyProvider } from "@/context/property"
 import { PropsWithChildren } from "react";
 
 export default function Providers({ children }: PropsWithChildren) {
     return (
         <UserProvider>
+            <PropertyProvider> 
             {children}
+            </PropertyProvider>
         </UserProvider>
     )
 } 
