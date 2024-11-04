3d6cb2e931c19239328546b47fda1341450ef39f
Elin Östman <elin.ostman@hotmail.com>
2024-11-04 10:41:09 +0100

    created footer component

--- src/components/content/footer.tsx
+++ src/components/content/footer.tsx
@@ -1,7 +1,11 @@
+"use client"
 import Link from 'next/link';
+import { usePathname } from "next/navigation"
 
 
 export default function Footer() {
+
+    const pathname = usePathname() 
 
     
     return (
@@ -11,7 +15,7 @@ export default function Footer() {
             <div className="container mx-auto flex flex-col md:flex-row items-start justify-between gap-8 shadow-t-lg">
                 <div className="flex flex-col justify-center md:w-1/2 space-y-4">
                     <h3 className=" text-4xl font-bold sm:text-3xl md:text-4xl lg:text-5xl">StayCation</h3>
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tempora repellat doloremque assumenda aliquam officiis quos at dignissimos aut, officia quam reprehenderit error maxime veniam quo. </p>
+                    <p> Stay updated with the latest staycation listings! Discover top local getaways, exclusive offers, and insider tips to make your next staycation an unforgettable adventure. Perfect escapes await right where you are! </p>
                     <div className="flex">
                         <div className="relative w-full pt-4"> 
                     <input 
@@ -23,14 +27,14 @@ export default function Footer() {
                 type="button" 
                 className="absolute top-1/2 right-0 transform -translate-y-1/2 mt-2 mr-2 px-4 py-2 text-black font-semibold transition-transform duration-300 text-sm hover:text-base"
             >
-                Sign Up
+                Stay Updated
             </button>
             </div>
                 </div>
                 </div>
                
                 <div className='flex flex-col space-y-2'>  
-                <p className="font-semibold">Hoosting</p>                  
+                <p className="font-semibold">Hosting</p>                  
                 <nav className="flex flex-col space-y-2">                    
                 <Link href={'#'}>How to list your home</Link>
                 <Link href={'#'}>Host Resources</Link>
@@ -42,10 +46,10 @@ export default function Footer() {
                 <div className='flex flex-col space-y-2'>  
                 <p className="font-semibold">Support</p>                  
                 <nav className="flex flex-col space-y-2">                    
-                   <Link href={'#'}>Guides</Link>
+                   <Link className={`${pathname === "/" ? "active" : ""}`} href={'#'}>Guides</Link>
                    <Link href={'#'}>Q&A</Link>
-                   <Link href={'#'}>Contact Us</Link>
                    <Link href={'#'}>Cancellation options</Link>
+                   <Link href={'#'}>Contact Us</Link>
                 </nav>
                 </div>
 
@@ -54,7 +58,6 @@ export default function Footer() {
                 <nav className="flex flex-col space-y-2">                    
                 <Link href={'#'}>The Staycation-Story</Link>
                 <Link href={'#'}>Leagally</Link>
-                <Link href={'#'}>Contact us</Link>
                 <Link href={'#'}>Socials</Link>
                 </nav>
                 </div>
